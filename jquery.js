e3b0f00bb2994f9fcbcc669f58125663b84fc3c4
Mori116 <cacaomame096@gmail.com>
2021-08-08 20:04:25 +0900

    [Add]css適用

--- jquery.js
+++ jquery.js
@@ -1,13 +1,33 @@
-// HTMLと紐づけする必要がある
-// HTMLの読み込みが終わってからjQueryが実行されるように記述する必要がある
+// // HTMLと紐づけする必要がある
+// // HTMLの読み込みが終わってからjQueryが実行されるように記述する必要がある
 
-$(document).ready(function () {
-  // $( セレクタ ).メソッド ( パラメータ );
-  $('body').html('<h1>Hello jQuery!!</h1>');
-});
-
-// 省略形
-// $(function(){
-//   jQueryプログラムの内容
+// $(document).ready(function () {
+//   // $( セレクタ ).メソッド ( パラメータ );
+//   $('body').html('<h1>Hello jQuery!!</h1>');
 // });
 
+// // 省略形
+// // $(function(){
+// //   jQueryプログラムの内容
+// // });
+
+// cssメソッド複数
+$(function(){
+  $('.box1').css({
+    'background-color': '#0000FF',
+    'height': '100px'
+  });
+});
+
+// cssメソッド１つの場合
+$(function(){
+  $('.box1').css('background-color','#0000FF');
+});
+
+$(function(){
+  $('.box1').slideDown();
+  // $('.box1').slideUp();
+  // $('.box1').hide();
+  // $('.box1').show();
+  // $('.box1').css({'background-color': '#FF0000'});
+});
